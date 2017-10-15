21725f6a2149e42a33b1aad94ae47049f41dafab
RSmetanyuk <rsmetanyuk@gmail.com>
2017-10-15 20:20:10 +0300

    Changed data.factory.js

--- js/data.factory.js
+++ js/data.factory.js
@@ -26,28 +26,3 @@ app.factory('DataFactory', function($http){
 
   return Data;
 });
-
-/*
-  Data.getApi = function(database) {      
-    let promise = new Promise(function(resolve, reject) {
-      if (Data[database] === undefined) { 
-        $http.get("https://footballbet.com.ua/api/".concat(database, "/"))
-          .then(function(response) {
-            Data[database] = response.data.result; 
-
-            if (database === "teams") {
-              for (let i in Data.teams) {
-                Data.teams[i].emblemaUrl = (Data.teams[i].emblema === "" ? "/img/no_logo.png" :
-                  "https://footballbet.com.ua/teams/embl/".concat(Data.teams[i].emblema));
-              }
-            }  
-
-
-          });
-      } else {
-        resolve();
-      }
-    });
-    return promise;
-  };
-  */
